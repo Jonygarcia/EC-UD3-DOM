a4ec04602c2a24da5911e50bb53d83d18eb99c93
Jonatan Garcia <90980367+Jonygarcia@users.noreply.github.com>
2022-12-21 23:15:24 +0100

    doe

---
+++ 021hideButton/021hideButton.js
@@ -0,0 +1,7 @@
+/*020hideTextButton.html/.js: Agrega JavaScript al button para hacer que <div 
+id="text"> desaparezca al hacerle click.*/
+"use strict";
+
+function hideButton(elem) {
+    elem.setAttribute("hidden", "true");
+}
